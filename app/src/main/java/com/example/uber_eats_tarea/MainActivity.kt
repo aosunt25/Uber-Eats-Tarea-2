fd99bb1c16ab8963bc8e78be3dc2bbd5f66700a5
Alfredo Osuna Torres <38539148+aosunt25@users.noreply.github.com>
2021-02-27 10:22:20 -0600

    Tarea terminada

--- app/src/main/java/com/example/uber_eats_tarea/MainActivity.kt
+++ app/src/main/java/com/example/uber_eats_tarea/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.uber_eats_tarea
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
+import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     var propina = 1.0f
 
     var total = 0.0f
+    var compra_id = 0
+    var listaCompra = mutableListOf<Orden>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +38,23 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                subTotlaTextView.text = p0.toString()
-                subtotal = p0.toString().toFloat()
+                if(!TextUtils.isEmpty(subtotalEditTextNumberDecimal.text.toString())){
+                    subTotlaTextView.text = p0.toString()
+                    subtotal = p0.toString().toFloat()
 
-                orderFee = subtotal * 0.02f
-                serviceFee = subtotal * 0.05f
-                deliverFee = subtotal * 0.1f
+                    orderFee = subtotal * 0.02f
+                    serviceFee = subtotal * 0.05f
+                    deliverFee = subtotal * 0.1f
 
-                orderFeeTextView.text = orderFee.toString()
-                serviceFeeTextView.text = serviceFee.toString()
-                valorPropinaTextView3.text = deliverFee.toString()
+                    orderFeeTextView.text = orderFee.toString()
+                    serviceFeeTextView.text = serviceFee.toString()
+                    valorPropinaTextView3.text = deliverFee.toString()
 
-                total = (subtotal + orderFee + serviceFee + deliverFee) * propina
+                    total = (subtotal + orderFee + serviceFee + deliverFee) * propina
 
-                placeOrderButton.text = total.toString()
+                    placeOrderButton.text = total.toString()
+                }
+
             }
 
         })
@@ -81,5 +89,19 @@ class MainActivity : AppCompatActivity() {
             placeOrderButton.text = total.toString()
         }
 
+
+    }
+    fun guardarOrden(view: View){
+        compra_id +=1
+        subtotal =  placeOrderButton.text.toString().toFloat()
+        var orden = Orden(compra_id, subtotal)
+        listaCompra.add(orden)
+        subtotalEditTextNumberDecimal.text.clear()
+        subTotlaTextView.text = "0"
+        placeOrderButton.text = "0"
+        orderFeeTextView.text = "0"
+        serviceFeeTextView.text = "0"
+        valorPropinaTextView3.text = "0"
+        Log.i("com.example.uber_eats_tarea.lista", listaCompra.toString())
     }
 }
---
+++ app/src/main/java/com/example/uber_eats_tarea/Orden.kt
@@ -0,0 +1,3 @@
+package com.example.uber_eats_tarea
+
+data class Orden(var compra_id: Int, var subtotal: Float)
